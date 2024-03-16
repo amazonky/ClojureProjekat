c21871e6e637dbfcfec3b97437e6e457b8fff90b
unknown <isidoraperovic@icloud.com>
2024-03-16 17:37:03 +0100

    Update code to include elapsed time in seconds

--- src/project_ml/core.clj
+++ src/project_ml/core.clj
@@ -4,28 +4,37 @@
   (+ (rand-int (- max-value min-value 1)) min-value))
 (defn -main [& args]
   (println "Welcome to Guess the Number!")
-  (let [min-value 1
+  (print "Please enter your name: ")
+  (flush)
+  (let [player-name (read-line)
+        min-value 1
         max-value 100
-        target (get-random-number min-value max-value)]
-    (loop [attempts 0]
+        target (get-random-number min-value max-value)
+        start-time (System/currentTimeMillis)]
+    (loop [attempts 0
+           game-data {:name player-name
+                      :attempts 0}]
       (if (= attempts 10)
         (do
           (println "You've run out of attempts! The number was" target)
           (println "Game Over!"))
         (let [guess (read-line)]
           (if (Integer/parseInt guess)
-            (let [guess (Integer/parseInt guess)]
+            (let [guess (Integer/parseInt guess)
+                  new-attempts (inc attempts)]
               (cond
                 (< guess target) (do
                                    (println "Too low!")
-                                   (recur (inc attempts)))
+                                   (recur new-attempts game-data))
                 (> guess target) (do
                                    (println "Too high!")
-                                   (recur (inc attempts)))
+                                   (recur new-attempts game-data))
                 :else (do
-                        (println "Congratulations! You guessed the number in" (inc attempts) "attempts!")
-                        (println "Game Over!"))))
-            (do
-              (println "Please enter a valid number!")
-              (recur attempts)))))))
-  )
+                        (let [end-time (System/currentTimeMillis)
+                              elapsed-time (- end-time start-time)]
+                        (println "Congratulations," player-name "! You guessed the number in" new-attempts "attempts!") 
+                          (println "Elapsed time:", elapsed-time "milliseconds")
+                        (let [updated-game-data (assoc game-data :attempts new-attempts
+                                                       :elapsed-time-milliseconds elapsed-time)]
+                          (println "Game Over!")
+                          (println "Game data:", updated-game-data))))))))))))
